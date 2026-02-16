c99ea5573712f26e5950a66e8fb1d068c5567a39
viveksitm13@gmail.com <viveksitm13@gmail.com>
2026-02-16 19:10:08 +0530

    hg

--- script/script.js
+++ script/script.js
@@ -95,5 +95,63 @@ AOS.init({
 });
 
 
+const statCounters = document.querySelectorAll(".stat-number");
+let statAnimated = false;
+
+const startStatCounting = () => {
+  statCounters.forEach((counter) => {
+
+    const originalText = counter.innerText.trim(); // "32K+" , "5+" , "50+"
+    const isK = originalText.toLowerCase().includes("k");
+
+    let target = parseInt(originalText.replace(/\D/g, ""));
+    if (isK) target = target * 1000;
+
+    let current = 0;
+
+    const updateCount = () => {
+      const increment = target / 200;
+
+      if (current < target) {
+        current += increment;
+
+        if (isK) {
+          counter.innerText = Math.floor(current / 1000) + "K+";
+        } else {
+          counter.innerText = Math.floor(current) + "+";
+        }
+
+        setTimeout(updateCount, 10);
+      } else {
+        // final value
+        if (isK) {
+          counter.innerText = (target / 1000) + "K+";
+        } else {
+          counter.innerText = target + "+";
+        }
+      }
+    };
+
+    counter.innerText = "0+";
+    updateCount();
+  });
+};
+
+const statObserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting && !statAnimated) {
+        startStatCounting();
+        statAnimated = true;
+      }
+    });
+  },
+  { threshold: 0.5 }
+);
+
+const statSection = document.querySelector(".stats-wrapper");
+statObserver.observe(statSection);
+
+
 
 
